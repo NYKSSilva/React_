7bcdafd4136a221fa04c4c662a31539ec90aed0f
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-03-03 11:12:43 -0300

    aula05-06

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/theme.css'
+import './styles/global.css'
+// import {Heading} from './component/Heading'
+export function App() {
+
+  return (
+    <>
+     <h1>Hello💕</h1>
+     {/* <Heading> Olá mundo</Heading> */}
+    </>
+  )
+}
+
+export default App
